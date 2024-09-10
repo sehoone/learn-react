ccc6c952bb1d76f085e02f8777037bba2b4625c5
SeHoon <sehoon1989@gmail.com>
2024-09-10 22:59:43 +0900

    feature/add-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from '@/routes';
+import store from './store';
+import React from 'react';
+import { Provider } from 'react-redux';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <RouterProvider router={router} />
-  )
+    <React.StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </React.StrictMode>
+  );
 }
 
-export default App
+export default App;
--- src/components/sample/StateComponent.tsx
+++ src/components/sample/StateComponent.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '@/store';
+import { increment, decrement, incrementByAmount } from '@/store/sample/counterSlice';
 
 const StateComponent = () => {
-    const [count, setCount] = useState(0);
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch();
 
     return (
         <div>
             <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <button onClick={() => dispatch(incrementByAmount(5))}>Increment by 5</button>
         </div>
     );
 };
