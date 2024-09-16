4428e593e4352a46390720d6f6268029c8beaebc
SeHoon <sehoon1989@gmail.com>
2024-09-17 00:47:49 +0900

    feature/fix-state-clean-up

--- src/components/tic-tac-toe/GameBoard.tsx
+++ src/components/tic-tac-toe/GameBoard.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { Turn, PlayerSymbol } from '@/pages/sample/tic-tac-toe/TicTacToePage';
 
-type PlayerSymbol = 'X' | 'O' | null;
+// type PlayerSymbol = 'X' | 'O' | null;
 
 const initialGameBoard: PlayerSymbol[][] = [
   [null, null, null],
@@ -9,23 +9,30 @@ const initialGameBoard: PlayerSymbol[][] = [
 ]
 
 type Props = {
-  onSelectSquare: () => void, activePlayerSymbol: PlayerSymbol
+  onSelectSquare: (rowIndex: number, cellIndex: number) => void,
+  turn: Turn[]
 }
 
 const GameBoard = (prop: Props) => {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  const gameBoard = initialGameBoard;
 
-  const handleSelectSquare = (rowIndex: number, cellIndex: number) => {
-    setGameBoard((prevGameBoard) => {
-      // 상태감지를 위해 깊은복사를 통해 독립적인 새로운 배열을 생성. 
-      const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-      updatedBoard[rowIndex][cellIndex] = prop.activePlayerSymbol;
-      return updatedBoard;
-
-    });
-
-    prop.onSelectSquare();
+  for (const turn of prop.turn) {
+    const { row, cell } = turn.square;
+    gameBoard[row][cell] = turn.player;
   }
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // const handleSelectSquare = (rowIndex: number, cellIndex: number) => {
+  //   setGameBoard((prevGameBo  ard) => {
+  //     // 상태감지를 위해 깊은복사를 통해 독립적인 새로운 배열을 생성. 
+  //     const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+  //     updatedBoard[rowIndex][cellIndex] = prop.activePlayerSymbol;
+  //     return updatedBoard;
+
+  //   });
+
+  //   prop.onSelectSquare();
+  // }
 
   return (
     <ol id='game-board'>
@@ -34,7 +41,7 @@ const GameBoard = (prop: Props) => {
           <ol>
             {row.map((playerSymbol, cellIndex) => (
               <li key={cellIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, cellIndex)}>{playerSymbol}</button>
+                <button onClick={() => prop.onSelectSquare(rowIndex, cellIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
--- src/pages/sample/tic-tac-toe/TicTacToePage.tsx
+++ src/pages/sample/tic-tac-toe/TicTacToePage.tsx
@@ -4,13 +4,37 @@ import './index.less';
 import Player from '@/components/tic-tac-toe/Player'
 import GameBoard from '@/components/tic-tac-toe/GameBoard';
 import { useState } from 'react';
-type PlayerSymbol = 'X' | 'O' | null;
+export type PlayerSymbol = 'X' | 'O' | null;
+export interface Turn {
+  square: { row: number; cell: number };
+  player: PlayerSymbol;
+}
+const deriveActivePlayer = (gameTurn: Turn[]): PlayerSymbol => {
+  let currentPlayer: PlayerSymbol = 'X';
+  if (gameTurn.length > 0 && gameTurn[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
 
 const TicTacToePage = () => {
-  const [activePlayer, setActivePlayer] = useState<PlayerSymbol>('X');
+  const [gameTurn, setGameTurn] = useState<Turn[]>([]);
+  // const [activePlayer, setActivePlayer] = useState<PlayerSymbol>('X');
+  const activePlayer = deriveActivePlayer(gameTurn);
 
-  const handleSelectSquare = () => {
-    setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X');
+  const handleSelectSquare = (rowIndex: number, cellIndex: number) => {
+    // setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X');
+
+    setGameTurn((prevGameTurn) => {
+      const currentPlayer = deriveActivePlayer(prevGameTurn);
+
+      const updatedTurns: Turn[] = [
+        { square: { row: rowIndex, cell: cellIndex }, player: currentPlayer },
+        ...prevGameTurn
+      ];
+      return updatedTurns;
+    });
   }
 
   return (
@@ -20,7 +44,7 @@ const TicTacToePage = () => {
           <Player name='Player1' symbol='X' isActive={activePlayer === 'X'} />
           <Player name='Player1' symbol='O' isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} turn={gameTurn} />
       </div>
     </main>
   )
